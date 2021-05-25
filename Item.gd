24b6d73aa3a4124c7bf2cfbd001eb003fcf9bdfc
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2021-05-25 21:38:54 +1000

    Add random + sprites

--- Item.gd
+++ Item.gd
@@ -1,5 +1,24 @@
 extends RigidBody2D
 
+onready var ItemSprite = $Sprite
+
+var type = null
+var food = preload("res://food.png")
+var bad = preload("res://bad.png")
+
+
+func init(type_param, speed_param):
+	type = type_param
+	gravity_scale = speed_param
+	set_sprite()
+
+
+func set_sprite():
+	if type == "bad":
+		ItemSprite.texture = bad
+	elif type == "food":
+		ItemSprite.texture = food
+
 
 func _on_Area2D_body_entered(_body):
 	# depending on what the current item is,
--- Spawner.gd
+++ Spawner.gd
@@ -1,22 +1,56 @@
 extends Node2D
 
 var Item = preload("res://Item.tscn")
+onready var Timer = $Timer
 
 var MARGIN = 200
 
+var MIN_SPAWN_TIME = 0.25
+var MAX_SPAWN_TIME = 2
+var MAX_SPEED = 6
+var RATE_GOOD = 0.5
+
+var rng = null
+
 
 func _ready():
-	randomize()
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
+	start_timer()
+
+
+func get_random_time():
+	return rng.randf_range(MIN_SPAWN_TIME, MAX_SPAWN_TIME)
+
+
+func get_random_speed():
+	return rng.randf_range(1, MAX_SPEED)
+
+
+func start_timer():
+	Timer.start(get_random_time())
+
+
+func get_random_position():
+	var random = rng.randi() % (int(get_viewport_rect().size.x) - MARGIN)
+	return Vector2(random + MARGIN / 2, 0)
+
+
+func get_random_type():
+	var random_float = rng.randf()
+	if random_float < RATE_GOOD:
+		return "food"
+	else:
+		return "bad"
 
 
 func spawn_random_item():
-	var random = randi() % (int(get_viewport_rect().size.x) - MARGIN)
 	var item = Item.instance()
-	# randomise item good/bad
-	# randomise timeout
-	item.position = Vector2(random + MARGIN / 2, 0)
+	item.position = get_random_position()
 	add_child(item)
+	item.init(get_random_type(), get_random_speed())
 
 
 func _on_Timer_timeout():
 	spawn_random_item()
+	start_timer()
