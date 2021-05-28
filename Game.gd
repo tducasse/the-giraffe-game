c55418e92260e06df52056272cfa01b9b5ebd853
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2021-05-28 23:22:10 +1000

    Add sounds

--- Game.gd
+++ Game.gd
@@ -4,12 +4,25 @@ extends Node2D
 onready var Growth = $Growth
 onready var Player = $Player
 
+onready var Music = $Music
+onready var Lost = $Lost
+onready var Win = $Win
+
+onready var Hurt = $Hurt
+onready var LevelUp = $LevelUp
+onready var LevelDown = $LevelDown
+onready var Pickup = $Pickup
+
 var HP_LOSS = 30
 var HP_GAIN = 50
 
 
 func _ready():
-	pass
+	Hurt.volume_db = -6
+	Pickup.volume_db = -6
+	LevelDown.volume_db = -6
+	LevelUp.volume_db = -6
+	Music.play()
 
 
 func _on_Spawner_item_picked(type):
@@ -23,6 +36,9 @@ func lose_hp():
 	Growth.value = max(0, Growth.value - HP_LOSS)
 	if Growth.value == 0:
 		shrink()
+	else:
+		Hurt.play()
+		
 
 
 func gain_hp():
@@ -30,19 +46,28 @@ func gain_hp():
 	if Growth.value == 100:
 		grow()
 		Growth.value = 0
+	else:
+		Pickup.play()
+		
 
 
 func grow():
+	LevelUp.play()
 	Player.add_neck()
 
 
 func shrink():
+	LevelDown.play()
 	Player.shrink()
 
 
 func _on_Player_end_game():
+	Music.stop()
+	Lost.play()
 	print("lost_game")
 
 
-func _on_Goal_item_picked(type):
+func _on_Goal_item_picked(_type):
+	Music.stop()
+	Win.play()
 	print("win game")
